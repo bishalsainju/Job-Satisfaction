212dd084b21d463b4ceb99d6dfbcd1e8bd11dd92
Bishal Sainju <bishalsainju@Bishals-iMac.local>
2019-08-16 10:28:47 -0600

    fortune 1000 scraping code

--- scraper/fortune500_test.py
+++ scraper/fortune500_test.py
@@ -1,11 +1,12 @@
-# from bs4 import BeautifulSoup
-# import requests
-#
-#
-# page_str = 'https://fortune.com/fortune500/2019/univision-communications'
-# # page_str = 'https://fortune.com/fortune500/2019/search/'
-#
-# page = requests.get(page_str)
+from bs4 import BeautifulSoup
+import requests
+
+
+page_str = 'https://fortune.com/fortune500/2019/univision-communications'
+# page_str = 'https://fortune.com/fortune500/2019/search/'
+
+page = requests.get(page_str)
+print(page)
 # soup = BeautifulSoup(page.text, features='html.parser')
 #
 # print(soup)
@@ -13,21 +14,21 @@
 # print(data)
 
 
-import json
-import re
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
-from bs4 import BeautifulSoup
-
-data = urllib2.urlopen('http://fortune.com/fortune500/')
-soup = BeautifulSoup(data)
-postid = next(attr for attr in soup.body['class'] if attr.startswith('postid'))
-postid = re.match(r'postid-(\d+)', postid).group(1)
-
-url = "http://fortune.com/data/franchise-list/{postid}/1/".format(postid=postid)
-data = json.load(urllib2.urlopen(url))
-
-resulting_url = urllib2.parse.urljoin(url, data['articles'][0]['url'])
-print(resulting_url)
+# import json
+# import re
+# try:
+#     import urllib.request as urllib2
+# except ImportError:
+#     import urllib2
+# from bs4 import BeautifulSoup
+#
+# data = urllib2.urlopen('http://fortune.com/fortune500/')
+# soup = BeautifulSoup(data)
+# postid = next(attr for attr in soup.body['class'] if attr.startswith('postid'))
+# postid = re.match(r'postid-(\d+)', postid).group(1)
+#
+# url = "http://fortune.com/data/franchise-list/{postid}/1/".format(postid=postid)
+# data = json.load(urllib2.urlopen(url))
+#
+# resulting_url = urllib2.parse.urljoin(url, data['articles'][0]['url'])
+# print(resulting_url)
--- scraper/website_generator.py
+++ scraper/website_generator.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import requests
+import config.config as config
 
-df = pd.read_csv('~/Downloads/Fortune50.csv')
-company_list = df['Company'].tolist()
+
+config = config.Config()
+project_root = config.project_root
+data_path = config.data_path
+
+df = pd.read_csv('~/Downloads/Fortune1000.csv')
+# df = pd.read_csv('~/Downloads/Fortune50.csv')
+company_list = df['Company Name'].tolist()
 
 lst = []
 for company in company_list:
-    company_name = company[company.find('.')+2:].strip().replace(' ', '-')
-    company_review_site = 'http://www.indeed.com/cmp/'+company_name+'/reviews'
+    company_name = company.strip().replace('&', '').replace(' ', '-').lower()
+    # company_review_site = 'http://www.indeed.com/cmp/'+company_name+'/reviews'
+    company_review_site = 'https://fortune.com/fortune500/2019/'+company_name
     request = requests.get(company_review_site)
     if request.status_code == 200:
         lst.append([company, company_name, company_review_site])
@@ -15,4 +23,4 @@ for company in company_list:
         print(company)
 
 df_site = pd.DataFrame(lst, columns =['Company', 'Company_Name', 'Company_Review_Site'])
-df_site.to_csv('review_site.csv', index=False)
+df_site.to_csv(data_path+'/scraper_data/review_site1000', index=False)
