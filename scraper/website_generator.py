28f86dd8576a6c758c3e4defd327b068f3d9c8e3
bishalsainju <bishalsainju7@gmail.com>
2019-11-22 20:48:49 -0700

    Changes

--- scraper/website_generator.py
+++ scraper/website_generator.py
@@ -7,20 +7,23 @@ config = config.Config()
 project_root = config.project_root
 data_path = config.data_path
 
-df = pd.read_csv('~/Downloads/Fortune1000.csv')
-# df = pd.read_csv('~/Downloads/Fortune50.csv')
-company_list = df['Company Name'].tolist()
+df = pd.read_csv(data_path +'/scraper_data/Fortune1000.csv')
 
+count = 0
 lst = []
-for company in company_list:
+for index, row in df.iterrows():
+    rank = int(row["Rank"])
+    company = row["Company Name"]
     company_name = company.strip().replace('&', '').replace(' ', '-').lower()
-    # company_review_site = 'http://www.indeed.com/cmp/'+company_name+'/reviews'
-    company_review_site = 'https://fortune.com/fortune500/2019/'+company_name
+    company_review_site = 'https://fortune.com/fortune500/2019/' + company_name
     request = requests.get(company_review_site)
     if request.status_code == 200:
-        lst.append([company, company_name, company_review_site])
+        lst.append([rank, company, company_name, company_review_site])
     else:
         print(company)
+    count += 1
+    if(count == 50):
+        break
 
-df_site = pd.DataFrame(lst, columns =['Company', 'Company_Name', 'Company_Review_Site'])
-df_site.to_csv(data_path+'/scraper_data/review_site1000', index=False)
+df_site = pd.DataFrame(lst, columns =['Rank', 'Company', 'Company_Name', 'Company_Review_Site'])
+df_site.to_csv(data_path+'/scraper_data/fortune_site50.csv', index=False)
